71898eb3384210996a87d1685a2a95d46532dede
Meng Luo <czheluo@gmail.com>
2019-09-16 08:55:21 +0800

    Update barplot.R

--- statsplot/barplot.R
+++ statsplot/barplot.R
@@ -1,13 +1,10 @@
 
 library(RColorBrewer)
-
 color = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
 rcolor<-color[sample(1:length(color),length(color))]
 
-
 pq_before_convert <- parental_quantities(dosage_matrix = all_genotype,
                                          parent1 = P1,parent2 = P2, las=2)
-
 
 png(paste("ALL segregation summary",".png",sep=""),width=1600, height=900)
 
@@ -91,7 +88,4 @@ bp = barplot(tmp, names=c("site 1", "site 2", "site 3", "site 4") )
 text(x=bp, y=tmp, labels=round(tmp,0), pos=3, xpd=NA)
 # numbers within bars
 text(x=bp, y=tmp, labels=round(tmp,0), pos=1)
-
-
-
 
