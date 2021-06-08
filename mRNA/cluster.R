10dcd09d341c30eb46f04af3e0763d617f4c79ee
Meng Luo <czheluo@gmail.com>
2021-06-08 17:47:00 +0800

    Create cluster.R

---
+++ mRNA/cluster.R
@@ -0,0 +1,98 @@
+require(ppclust)
+require(factoextra)
+require(dplyr)
+require(cluster)
+require(fclust)
+require(fields)
+data(iris)
+x=iris[,-5]
+x[1:5,]
+
+c1<-read.table("cluster_1",header=T,row.names=1,sep="\t")
+c1<-read.table("new_cluster_2",header=T,row.names=1,sep="\t")
+c1<-read.table("new_cluster_3",header=T,row.names=1,sep="\t")
+#x<-as.matrix(t(c1))
+res.fcm <- fcm(c1, centers=5)
+membership<-res.fcm$u[,1]
+mem=as.matrix(as.numeric(membership))
+colnames(mem)="mem"
+c=cbind(c1,mem)
+c1=c
+c1=c1[order(c1[,'mem']),]
+#colorbar.plot( 2, 75, y, horizontal=FALSE, col=rainbow(105))
+#plot(c(1:5),c1[1,],type="l") 
+pdf("cluster_3.pdf",width=10,height=10)
+par(mar=c(4.5, 4.5, 1, 6))
+plot(c(1:5),c1[1,c(1:5)],xlim=c(0.5,5), ylim=c(-2,1),type="l",col=rainbow(105)[1],axes=FALSE, ann=FALSE)
+for (i in 2:105){# Draw first line
+	lines(c(1:5),c1[i,c(1:5)], type = "l", col = rainbow(105)[i])                   # Add second line
+}  # Add third line
+
+axis(1, at=1:5, lab=c("D1","W2","W4","W8","W12"))
+# Make x axis using Mon-Fri labels
+
+# Make y axis with horizontal labels that display ticks at 
+# every 4 marks. 4*0:g_range[2] is equivalent to c(0,4,8,12).
+axis(2, las=1)
+
+# Create box around plot
+#box()
+# Label the x and y axes with dark green text
+title(xlab="Stages", col.lab=rgb(0,0.5,0))
+title(ylab="expression level", col.lab=rgb(0,0.5,0))
+box()
+
+#library(s2dverification)
+#lims <- seq(-1, 1, 0.2)
+#ColorBar(c1[,6], cols=rainbow(105))
+zr<- range(c1[,6])
+image.plot( legend.only=TRUE, zlim= zr,col=rainbow(105)) 
+
+dev.off()
+
+png("cluster_3.png",width=1000,height=800)
+par(mar=c(4.5, 4.5, 1, 6))
+plot(c(1:5),c1[1,c(1:5)],xlim=c(0.5,5), ylim=c(-2,1),type="l",col=rainbow(105)[1],axes=FALSE, ann=FALSE)
+for (i in 2:105){# Draw first line
+	lines(c(1:5),c1[i,c(1:5)], type = "l", col = rainbow(105)[i])                   # Add second line
+}  # Add third line
+
+axis(1, at=1:5, lab=c("D1","W2","W4","W8","W12"))
+# Make x axis using Mon-Fri labels
+
+# Make y axis with horizontal labels that display ticks at 
+# every 4 marks. 4*0:g_range[2] is equivalent to c(0,4,8,12).
+axis(2, las=1)
+
+# Create box around plot
+#box()
+# Label the x and y axes with dark green text
+title(xlab="Stages", col.lab=rgb(0,0.5,0))
+title(ylab="expression level", col.lab=rgb(0,0.5,0))
+box()
+
+#library(s2dverification)
+#lims <- seq(-1, 1, 0.2)
+#ColorBar(c1[,6], cols=rainbow(105))
+zr<- range(c1[,6])
+image.plot( legend.only=TRUE, zlim= zr,col=rainbow(105)) 
+
+dev.off()
+
+
+
+text(0,1,"Christmas strip", cex=2)
+library(fields)
+colorbar.plot(6, 0, c1[,6], horizontal=FALSE, col=rainbow(105),legend.only=TRUE)
+
+text(0,1,"Christmas strip", cex=2)
+# NOT RUN {
+# set up a plot but don't plot points  and no "box"
+plot( 1:10, (1:10)*10, type="n", bty="n") 
+# of course this could be anything 
+
+y<- cbind( 1:15, (1:15)+25)
+
+colorbar.plot( 2.5, 30, y)
+points( 2.5,30, pch="+", cex=2, adj=.5)
+
